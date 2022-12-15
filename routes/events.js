83e64342bd6a5646b45187eabc87e7587022206b
Juan Camilo Garcés <camo0402@gmail.com>
2022-12-14 23:45:14 -0500

    Date validation using moment and checking fields before posting Events

--- routes/events.js
+++ routes/events.js
@@ -4,6 +4,8 @@
 */
 
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { fieldValidator } = require("../middlewares/fields-validators");
 const { jwtValidator } = require("../middlewares/jwt-validator");
 const {
   getEvents,
@@ -11,6 +13,7 @@ const {
   updateEvent,
   deleteEvent,
 } = require("../controllers/events");
+const { isDate } = require("../helpers/isDate");
 const router = Router();
 
 // All routes need JWT validation
@@ -18,7 +21,16 @@ router.use(jwtValidator);
 
 router.get("/", getEvents);
 
-router.post("/", createEvent);
+router.post(
+  "/",
+  [
+    check("title", "Title is required").not().isEmpty(),
+    check("start", "Start date is required").custom(isDate),
+    check("end", "End date is required").custom(isDate),
+    fieldValidator,
+  ],
+  createEvent
+);
 
 router.put("/:id", updateEvent);
 
